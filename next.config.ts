04faef9949a7a0b40e2d35c947bac3eba696fd27
Tenzin Lama <lamagurutenzin@gmail.com>
2026-07-18 00:08:10 +0530

    Update Next.js for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-
-  eslint:{
-    ignoreDuringBuilds:true
-  }
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
